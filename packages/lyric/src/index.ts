bd5baa74847b7dd3be625929b1a86049342b0c71
Linho <lhp@linho.cc>
2026-04-05 14:27:52 +0800

    feat: QRC 加解密 TS 实现
    迁移自 https://github.com/apoint123/qrc-decoder

--- packages/lyric/src/index.ts
+++ packages/lyric/src/index.ts
@@ -2,6 +2,7 @@ export { parseLRC, stringifyLRC } from "./formats/lrc";
 export { parseLRCa2, stringifyLRCa2 } from "./formats/lrca2";
 export { parseYRC, stringifyYRC } from "./formats/yrc";
 export { parseQRC, stringifyQRC } from "./formats/qrc";
+export { decryptQrcHex, encryptQrcHex } from "./formats/eqrc";
 export { parseLYL, stringifyLYL } from "./formats/lyl";
 export { parseLYS, stringifyLYS } from "./formats/lys";
 export { parseLQE, stringifyLQE } from "./formats/lqe";
