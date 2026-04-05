a41308e6df87216cb382db7a7b73ecbdc1c47092
Linho <lhp@linho.cc>
2026-04-05 14:40:42 +0800

    feat: ASS 格式 TS 实现

--- packages/lyric/src/index.ts
+++ packages/lyric/src/index.ts
@@ -7,6 +7,7 @@ export { parseLYL, stringifyLYL } from "./formats/lyl";
 export { parseLYS, stringifyLYS } from "./formats/lys";
 export { parseLQE, stringifyLQE } from "./formats/lqe";
 export { parseESLRC, stringifyESLRC } from "./formats/eslrc";
+export { stringifyASS } from "./formats/ass";
 export { parseTTML, stringifyTTML } from "./formats/ttml";
 
 export type { LyricLine, LyricWord, TTMLLyric } from "./types";
